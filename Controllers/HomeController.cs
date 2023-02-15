c9e72a87e5cba1a15ca553a985a5b08455042557
christal <chambost.christal@gmail.com>
2023-02-15 09:31:23 +0100

    add css and layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace AppProjetFilRouge.Controllers
 			return View();
 		}
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
-
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
